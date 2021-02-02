9f193d5414133a3378efcd54e336b862041091c0
Dmitri So <dmitriso@yahoo.com>
2021-02-02 16:05:04 -0500

    Created SearchForm Components.

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,2 +1,20 @@
 import React from "react";
 
+function SearchForm() {
+    return (
+        <div class="container">
+            <nav class="navbar navbar-light bg-light">
+                <span class="navbar-brand mb-0 h1">Employee-Index</span>
+            </nav>
+            <nav class="navbar navbar-light bg-light">
+                <form class="form-inline">
+                    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
+                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                </form>
+            </nav>
+        </div>
+
+    );
+}
+
+export default SearchForm;
