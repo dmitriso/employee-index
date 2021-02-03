527e8ad8c6083d049ad0258550f651da9b304292
Dmitri So <dmitriso@yahoo.com>
2021-02-03 16:08:11 -0500

    Added jsx code for a search form html.

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -2,7 +2,23 @@ import React from "react";
 
 function SearchForm(props) {
     return (
-
+        <form className="container">
+        <div className="form-group text-center">
+          <h1 className="card">Employee-Index</h1>
+          <input
+            onChange={props.handleInputChange}
+            value={props.search}
+            name="search"
+            type="text"
+            className="form-control"
+            placeholder="Search For An Employee"
+          />
+          <br />
+          <button onClick={props.handleFormSubmit} className="btn btn-primary">
+            Search
+          </button>
+        </div>
+      </form>
 
     );
 }
