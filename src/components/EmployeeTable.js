c88e6c5d0a6c5255e9f9403cafccbdd73fca11f4
Dmitri So <dmitriso@yahoo.com>
2021-02-03 16:11:44 -0500

    Added jsx code for rendering the employee table and search form.

--- src/components/EmployeeTable.js
+++ src/components/EmployeeTable.js
@@ -9,7 +9,38 @@ class EmployeeTable extends Component {
 
     render() {
         return (
-
+            <div className="container">
+                <SearchForm
+                    handleFormSubmit={this.handleFormSubmit}
+                    handleInputChange={this.handleInputChange}
+                />
+               
+                    <div className="container">
+                    <table className="table table-striped">
+                        <thead>
+                            <tr>
+                                <th scope="col">Image</th>
+                                <th scope="col">Name</th>
+                                <th scope="col">Phone</th>
+                                <th scope="col">Email</th>
+                                <th scope="col">D.O.B.</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                        {this.state.employees.map(employee => (
+                            <tr>
+                                <td><img src={employee.picture.large} alt="Employee" /></td>
+                                <td>{employee.name.first} {employee.name.last}</td>
+                                <td>{employee.phone}</td>
+                                <td>{employee.email}</td>
+                                <td>{(employee.dob.date).substring(0, 10)}</td>
+                            </tr>
+                        ))}
+                        </tbody>
+                    </table>
+                    </div>
+                
+            </div>
         )
     }
 }
