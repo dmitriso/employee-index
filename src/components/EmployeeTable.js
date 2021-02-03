6ad1de5affa12c6c1e637eef774a00069bae80a2
Dmitri So <dmitriso@yahoo.com>
2021-02-03 16:10:55 -0500

    Added EmployeeTable.js file/Created class for EmployeeTable to render jsx code.

---
+++ src/components/EmployeeTable.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import API from "../utils/API";
+import SearchForm from "./SearchForm";
+
+
+
+class EmployeeTable extends Component {
+
+
+    render() {
+        return (
+
+        )
+    }
+}
+
+export default EmployeeTable;
