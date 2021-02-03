0ab1258adfa3a2a1bd28faf8c68193a8db08012b
Dmitri So <dmitriso@yahoo.com>
2021-02-03 16:07:10 -0500

    Added SearchForm.js file/Created SearchForm function to store jsx code.

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,18 +1,8 @@
 import React from "react";
 
-function SearchForm() {
+function SearchForm(props) {
     return (
-        <div class="container">
-            <nav class="navbar navbar-light bg-light">
-                <span class="navbar-brand mb-0 h1">Employee-Index</span>
-            </nav>
-            <nav class="navbar navbar-light bg-light">
-                <form class="form-inline">
-                    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
-                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-                </form>
-            </nav>
-        </div>
+
 
     );
 }
