094605ff5ac4f5bdb78d0308ea8a8c4a82417dbb
Dmitri So <dmitriso@yahoo.com>
2021-02-04 16:05:09 -0500

    Added buttons for sorting the table by age.

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+// SEARCH FORM COMPONENT
 function SearchForm(props) {
     return (
         <form className="container">
@@ -12,10 +12,17 @@ function SearchForm(props) {
             type="text"
             className="form-control"
             placeholder="Search For An Employee"
+            id="search"
           />
           <br />
           <button onClick={props.handleFormSubmit} className="btn btn-primary">
             Search
+          </button>
+          <button onClick={props.handleSortByAgeAscend} className="btn btn-primary">
+            Sort By Youngest
+          </button>
+          <button onClick={props.handleSortByAgeDescend} className="btn btn-primary">
+            Sort By Oldest
           </button>
         </div>
       </form>
